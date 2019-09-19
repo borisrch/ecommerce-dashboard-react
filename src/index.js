469cd50694633801bfa874f916edffb11bd9b0d0
Susie D'Arcy <borisc1996@gmail.com>
2019-09-19 10:39:11 +0100

    add rootReducer boilerplate

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from './store/reducers/rootReducer';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(
-  (state = {}) => state,
+  rootReducer,
   applyMiddleware(thunk)
 )
 
---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import message from './message';
+
+export default combineReducers({
+  message
+});
