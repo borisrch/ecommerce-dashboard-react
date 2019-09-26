8f70dc7e45460bb0fb0d6f85113f3e4ae7b880a0
Susie D'Arcy <borisc1996@gmail.com>
2019-09-26 13:07:56 +0100

    add FieldModal component as dialog to modify product info

--- src/Inventory/ProductModal.js
+++ src/Inventory/ProductModal.js
@@ -5,12 +5,13 @@ import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { Container } from '@material-ui/core';
+import { Container, Modal } from '@material-ui/core';
 
 import UpdateProduct from './UpdateProduct';
 import UpdateProductStock from './UpdateProductStock';
 import RemoveProduct from './RemoveProduct';
 import FieldRow from './../Common/FieldRow';
+import FieldModal from './../Common/FieldModal';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -38,12 +39,51 @@ const useStyles = makeStyles(theme => ({
     padding: 15,
     marginBottom: theme.spacing(2),
     borderRadius: 4
-  }
+  },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    // boxShadow: theme.shadows[5],
+    boxShadow: '0 20px 60px -2px rgba(27,33,58,.4)',
+    padding: theme.spacing(2, 4, 3),
+    outline: 'none',
+    borderRadius: '8px'
+  },
 }));
 
 
-export default function Search(props) {
+export default function ProductModal(props) {
   const classes = useStyles();
+
+  const [fieldModal, setFieldModal] = React.useState({
+    open: false,
+    field: {
+      label: null,
+      value: null
+    }
+  });
+
+  const [product, setProduct] = React.useState({
+    ...props.item
+  })
+
+  const showFieldModal = (field) => {
+    setFieldModal({
+      open: true,
+      field,
+    });
+  }
+
+  const handleClose = () => {
+    setFieldModal({
+      ...fieldModal,
+      open: false
+    });
+  }
 
   return (
     <Container className={classes.container}>
@@ -55,19 +95,36 @@ export default function Search(props) {
       </Grid>
 
       <Box>
-        <FieldRow label="Name" value={props.item.name} onClick={() => { alert('hi') }} />
-        <FieldRow label="Brand" value={props.item.brand} onClick={() => { alert('hi') }} />
-        <FieldRow label="Description" value={props.item.description} onClick={() => { alert('hi') }} />
-        <FieldRow label="Category" value={props.item.category} onClick={() => { alert('hi') }} />
+        <FieldRow label="Name" value={props.item.name} openModal={showFieldModal} />
+        <FieldRow label="Brand" value={props.item.brand} openModal={showFieldModal} />
+        <FieldRow label="Description" value={props.item.description} openModal={showFieldModal} variant="textarea" />
+        <FieldRow label="Category" value={props.item.category} openModal={showFieldModal} />
         <FieldRow label="ID" value={props.item.id} />
         <FieldRow label="Stock" value={Math.floor(Math.random() * 100)} />
       </Box>
 
       <Box display="flex" justifyContent="flex-end" style={{ marginTop: '2em' }}>
-        <Button vsize="small" color="primary" className={classes.button} style={{ marginRight: 10 }} onClick={props.onClose}>
-          Done
+        <Button variant="contained" color="primary" className={classes.button} style={{ marginRight: 10 }} onClick={props.onClose}>
+          Save
         </Button>
       </Box>
+
+      <Modal
+        disableAutoFocus={true}
+        disableBackdropClick
+        closeAfterTransition
+        open={fieldModal.open}
+        onClose={handleClose}
+        className={classes.modal}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <div style={{ outline: 'none' }}>
+          <FieldModal label={fieldModal.field.label} value={fieldModal.field.value} onClose={handleClose} item={props.item} setProduct={props.setProduct} />
+        </div>
+
+      </Modal>
 
     </Container>
   );
