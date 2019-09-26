975c8a7bb60772c9975df4d913e050972bd61385
Susie D'Arcy <borisc1996@gmail.com>
2019-09-26 13:04:37 +0100

    boilerplate for form used to create new product

---
+++ src/Inventory/CreateProductForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Grid, Typography } from '@material-ui/core';
+
+export default function CreateProductForm() {
+  return (
+    <Grid container>
+      <Grid item xs={4}>
+        <Typography variant="subtitle1">testsetestsetest</Typography>
+      </Grid>
+      <Grid item xs={8}>
+        <Typography variant="body1">bobobobobob</Typography>
+      </Grid>
+    </Grid>
+  )
+}
