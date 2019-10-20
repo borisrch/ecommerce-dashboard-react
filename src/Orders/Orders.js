d30d37f37cbb303e39afd4698952ae89da825c48
borisc1996 <borisc1996@gmail.com>
2019-10-20 16:05:28 -0400

    add toolbar to Orders component

--- src/Orders/Orders.js
+++ src/Orders/Orders.js
@@ -6,6 +6,11 @@ import OrdersTable from './OrdersTable';
 import Search from './Search';
 import SideBar from './Sidebar';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import RefreshIcon from '@material-ui/icons/Refresh';
+import SortIcon from '@material-ui/icons/Sort';
 
 const drawerWidth = 210;
 
@@ -22,27 +27,69 @@ const useStyles = makeStyles(theme => ({
   title: {
     fontFamily: 'ApercuMedium',
     marginTop: theme.spacing(4),
-    marginBottom: theme.spacing(4)
+    marginBottom: theme.spacing(4),
+    color: '#525f7f'
   },
   paper: {
     boxShadow: '0 0 1px 0 rgba(0,0,0,.22)',
+  },
+  toolbar: {
+    boxShadow: "0 0 11px #eaf0f6",
+    display: "inline-block",
+    marginBottom: theme.spacing(1),
+    width: "100%"
+  },
+  button: {
+    margin: theme.spacing(1)
+  },
+  action: {
+    marginLeft: "auto",
+    marginTop: "0.8rem",
+    marginRight: theme.spacing(2)
+  },
+  lastUpdated: {
+    marginTop: theme.spacing(2),
+    padding: 0,
+    color: "rgb(112, 117, 122)"
   }
 }));
 
 export default function Orders() {
   const classes = useStyles();
 
+  const [lastUpdatedTime, setLastUpdatedTime] = React.useState("N/A");
+
+  React.useEffect(() => {
+    // props.dispatch(fetchProducts());
+    setLastUpdatedTime(`${new Date().toLocaleString()}`);
+  }, []);
+
   return (
     <React.Fragment>
       <Container maxWidth="lg">
-        <Grid container spacing={2}>
-          <Grid item xs={8}>
-          </Grid>
-          <Grid item xs={4}>
-          </Grid>
-        </Grid>
         <Typography variant="h6" className={classes.title} gutterBottom>Orders</Typography>
+
+        <Paper className={classes.toolbar}>
+          <div style={{ display: "flex" }}>
+            <div>
+              <IconButton className={classes.button} color="primary">
+                <SortIcon />
+              </IconButton>
+              <IconButton className={classes.button}>
+                <RefreshIcon />
+              </IconButton>
+            </div>
+            <div className={classes.action}>
+              <Button>Test</Button>
+            </div>
+          </div>
+        </Paper>
         <OrdersTable />
+        <Container className={classes.lastUpdated}>
+          <Typography variant="overline">
+            Orders up to date. Last retrieved at {lastUpdatedTime}
+          </Typography>
+        </Container>
       </Container>
     </React.Fragment>
   );
