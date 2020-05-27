7f0779e5cca8aec63cc1ceea1b873c0ebce1f246
Boris Chan <borisc1996@gmail.com>
2020-05-27 00:06:59 -0400

    new login screen splash

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
-import Container from '@material-ui/core/Container';
-import { makeStyles } from '@material-ui/core/styles';
-import { Grid } from '@material-ui/core';
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
 
-import LoginCard from './LoginCard';
-import Welcome from '../Common/img/welcome.png';
-import { userSignInRequest } from '../store/actions/auth';
+import LoginCard from "./LoginCard";
+import Welcome from "../Common/img/welcome.svg";
+import { userSignInRequest } from "../store/actions/auth";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   image: {
-    width: '100%'
+    width: "100%",
+    pointerEvents: "none",
+    userSelect: "none",
   },
 }));
 
@@ -23,20 +25,26 @@ function Login(props) {
 
   return (
     <Container style={{}}>
-      <Grid container direction="row" justify="center" alignItems="center" style={{ height: '100vh' }}>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        style={{ height: "100vh" }}
+      >
         <Grid item xs={5}>
           <LoginCard userSignInRequest={userSignInRequest} />
         </Grid>
         <Grid item xs={7}>
-          <img className={classes.image} src={Welcome} />
+          <img className={classes.image} src={Welcome} draggable="false" />
         </Grid>
       </Grid>
     </Container>
-  )
+  );
 }
 
 Login.propTypes = {
-  userSignInRequest: PropTypes.func.isRequired
-}
+  userSignInRequest: PropTypes.func.isRequired,
+};
 
 export default connect(null, { userSignInRequest })(Login);
