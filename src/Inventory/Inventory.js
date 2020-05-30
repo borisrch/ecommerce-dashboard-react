3c76f1888f4c48ee09941347143e51053b35f299
Boris Chan <borisc1996@gmail.com>
2020-05-30 18:09:24 -0400

    remove unused imports

--- src/Inventory/Inventory.js
+++ src/Inventory/Inventory.js
@@ -8,10 +8,7 @@ import IconButton from "@material-ui/core/IconButton";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
-import SearchIcon from "@material-ui/icons/Search";
-import ViewModuleIcon from "@material-ui/icons/ViewModule";
-import ViewHeadlineIcon from "@material-ui/icons/ViewHeadline";
-import RefreshIcon from "@material-ui/icons/Refresh";
+
 import Fab from "@material-ui/core/Fab";
 import Zoom from "@material-ui/core/Zoom";
 import Modal from "@material-ui/core/Modal";
@@ -19,6 +16,11 @@ import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
+
+import SearchIcon from "@material-ui/icons/Search";
+import ViewModuleIcon from "@material-ui/icons/ViewModule";
+import ViewHeadlineIcon from "@material-ui/icons/ViewHeadline";
+import RefreshIcon from "@material-ui/icons/Refresh";
 
 import CreateProduct from "./CreateProduct";
 import InventoryItem from "./InventoryItem";
@@ -28,24 +30,24 @@ import ProductModal from "./ProductModal";
 import CreateProductForm from "./CreateProductForm";
 import PageTitle from "./../Common/PageTitle";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   fab: {
     margin: 0,
     top: "auto",
     left: "auto",
     position: "fixed",
     bottom: theme.spacing(7),
-    right: theme.spacing(7)
+    right: theme.spacing(7),
   },
   action: {
     marginLeft: "auto",
     marginTop: "0.8rem",
-    marginRight: theme.spacing(2)
+    marginRight: theme.spacing(2),
   },
   modal: {
     display: "flex",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   createProductModal: {
     display: "flex",
@@ -59,38 +61,38 @@ const useStyles = makeStyles(theme => ({
     boxShadow: "0 20px 60px -2px rgba(27,33,58,.4)",
     padding: theme.spacing(2, 4, 3),
     outline: "none",
-    borderRadius: "8px"
+    borderRadius: "8px",
   },
   emptyIcon: {
     color: "#00000032",
-    fontSize: "10em"
+    fontSize: "10em",
   },
   emptyContainer: {
-    marginTop: "25vh"
+    marginTop: "25vh",
   },
   title: {
     fontFamily: "ApercuMedium",
     marginTop: theme.spacing(4),
-    marginBottom: theme.spacing(4)
+    marginBottom: theme.spacing(4),
   },
   button: {
-    margin: theme.spacing(1)
+    margin: theme.spacing(1),
   },
   toolbar: {
     // boxShadow: '0 0 1px 0 rgba(0,0,0,.22)',
     boxShadow: "0 0 11px #eaf0f6",
     display: "inline-block",
     marginBottom: theme.spacing(1),
-    width: "100%"
+    width: "100%",
   },
   lastUpdated: {
     marginTop: theme.spacing(2),
     padding: 0,
-    color: "rgb(112, 117, 122)"
-  }
+    color: "rgb(112, 117, 122)",
+  },
 }));
 
-const Inventory = props => {
+const Inventory = (props) => {
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
@@ -98,7 +100,7 @@ const Inventory = props => {
     id: "ID",
     name: "Name",
     type: "Type",
-    description: "Description"
+    description: "Description",
   });
 
   const [searchModal, setSearchModal] = React.useState(false);
@@ -126,7 +128,7 @@ const Inventory = props => {
     setSearchModal(false);
   };
 
-  const handleOpen = product => {
+  const handleOpen = (product) => {
     setProduct(product);
     setOpen(true);
   };
@@ -137,13 +139,13 @@ const Inventory = props => {
 
   const transitionDuration = {
     enter: theme.transitions.duration.enteringScreen,
-    exit: theme.transitions.duration.leavingScreen
+    exit: theme.transitions.duration.leavingScreen,
   };
 
   return (
     <React.Fragment>
       <Container maxWidth="lg">
-        <PageTitle title="Inventory"/>
+        <PageTitle title="Inventory" />
         <Paper className={classes.toolbar}>
           <div style={{ display: "flex" }}>
             <div>
@@ -165,27 +167,27 @@ const Inventory = props => {
         {props.products.length === 0 || props.products.length === null ? (
           <EmptyInventory />
         ) : (
-            <React.Fragment>
-              <Grid container spacing={2}>
-                {props.products.map(product => (
-                  <Grid item xs={4} key={product.id}>
-                    <InventoryItem item={product} openModal={handleOpen} />
-                  </Grid>
-                ))}
-              </Grid>
-              <Container className={classes.lastUpdated}>
-                <Typography variant="overline">
-                  Inventory up to date. Last retrieved at {lastUpdatedTime}
-                </Typography>
-              </Container>
-            </React.Fragment>
-          )}
+          <React.Fragment>
+            <Grid container spacing={2}>
+              {props.products.map((product) => (
+                <Grid item xs={4} key={product.id}>
+                  <InventoryItem item={product} openModal={handleOpen} />
+                </Grid>
+              ))}
+            </Grid>
+            <Container className={classes.lastUpdated}>
+              <Typography variant="overline">
+                Inventory up to date. Last retrieved at {lastUpdatedTime}
+              </Typography>
+            </Container>
+          </React.Fragment>
+        )}
       </Container>
 
       <Zoom
         timeout={transitionDuration}
         style={{
-          transitionDelay: `${transitionDuration.exit}ms`
+          transitionDelay: `${transitionDuration.exit}ms`,
         }}
         in={true}
         unmountOnExit
@@ -207,7 +209,7 @@ const Inventory = props => {
         onClose={handleClose}
         BackdropComponent={Backdrop}
         BackdropProps={{
-          timeout: 500
+          timeout: 500,
         }}
         closeAfterTransition
         disableBackdropClick
@@ -227,7 +229,7 @@ const Inventory = props => {
         onClose={closeSearchModal}
         BackdropComponent={Backdrop}
         BackdropProps={{
-          timeout: 500
+          timeout: 500,
         }}
         closeAfterTransition
         disableBackdropClick
@@ -247,7 +249,7 @@ const Inventory = props => {
         onClose={closeSearchModal}
         BackdropComponent={Backdrop}
         BackdropProps={{
-          timeout: 500
+          timeout: 500,
         }}
         closeAfterTransition
         disableBackdropClick
@@ -263,20 +265,17 @@ const Inventory = props => {
 };
 
 Inventory.defaultProps = {
-  products: []
+  products: [],
 };
 
 Inventory.propTypes = {
-  products: PropTypes.array.isRequired
+  products: PropTypes.array.isRequired,
 };
 
 function mapStateToProps(state) {
   return {
-    products: state.product.products
+    products: state.product.products,
   };
 }
 
-export default connect(
-  mapStateToProps,
-  null
-)(Inventory);
+export default connect(mapStateToProps, null)(Inventory);
