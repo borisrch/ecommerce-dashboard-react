287ddaeb68066bb9d6dde66f555aa7d3e8118b0e
Boris Chan <borisc1996@gmail.com>
2020-05-30 18:09:57 -0400

    temporarily adding secondary button for UI test

--- src/Inventory/CreateProduct.js
+++ src/Inventory/CreateProduct.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import Icon from '@material-ui/core/Icon';
-import AddIcon from '@material-ui/icons/Add';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import Icon from "@material-ui/core/Icon";
+import AddIcon from "@material-ui/icons/Add";
+import PublishIcon from "@material-ui/icons/Publish";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   button: {
-    boxShadow: 'none',
-    fontFamily: 'ApercuMedium',
+    boxShadow: "none",
+    fontFamily: "ApercuMedium",
   },
   leftIcon: {
     marginRight: theme.spacing(1),
@@ -24,9 +25,26 @@ export default function CreateProduct(props) {
   const classes = useStyles();
 
   return (
-    <Button variant="contained" color="primary" className={classes.button} onClick={props.createProduct}>
-      Create New Product
-      <AddIcon className={classes.rightIcon}>Create New Product</AddIcon>
-    </Button>
-  )
+    <React.Fragment>
+      <Button
+        variant="outlined"
+        color="primary"
+        className={classes.button}
+        onClick={props.createProduct}
+        style={{ marginRight: "1em" }}
+      >
+        Import Product
+        <PublishIcon className={classes.rightIcon}>Import Product</PublishIcon>
+      </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        className={classes.button}
+        onClick={props.createProduct}
+      >
+        Create New Product
+        <AddIcon className={classes.rightIcon}>Create New Product</AddIcon>
+      </Button>
+    </React.Fragment>
+  );
 }
