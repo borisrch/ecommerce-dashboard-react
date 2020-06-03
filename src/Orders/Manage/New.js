6a746685b0fbe732c17366eb9d3e46825512f0e0
Boris Chan <borisc1996@gmail.com>
2020-06-02 22:43:50 -0400

    add component to represent "New" lifecycle stage

---
+++ src/Orders/Manage/New.js
@@ -0,0 +1,207 @@
+import React from "react";
+import clsx from "clsx";
+import {
+  makeStyles,
+  Container,
+  Typography,
+  TextField,
+  Box,
+  Button,
+  IconButton,
+  Popover,
+} from "@material-ui/core";
+
+import DoneIcon from "@material-ui/icons/Done";
+import PostAdd from "@material-ui/icons/PostAdd";
+
+const useStyles = makeStyles((theme) => ({
+  textfield: {
+    marginRight: theme.spacing(2),
+    marginBottom: theme.spacing(3),
+  },
+  root: {
+    padding: theme.spacing(4),
+  },
+  title: {
+    fontFamily: "ApercuMedium",
+    color: theme.palette.action.active,
+    marginBottom: theme.spacing(2),
+  },
+  subtitle: {
+    fontFamily: "ApercuMedium",
+    color: theme.palette.action.active,
+    marginBottom: theme.spacing(2),
+    marginTop: theme.spacing(2),
+  },
+  button: {
+    boxShadow: "none",
+    fontFamily: "ApercuMedium",
+  },
+  rightIcon: {
+    marginLeft: theme.spacing(1),
+  },
+  active: {
+    color: theme.palette.primary.main,
+  },
+  popover: {
+    padding: theme.spacing(2),
+  },
+}));
+
+export default function New(props) {
+  const classes = useStyles();
+
+  // TODO: Import the schema instead of manually deconstructing
+  const {
+    orderId,
+    created,
+    firstName,
+    lastName,
+    email,
+    address,
+    city,
+    country,
+    zip,
+    fulfillment,
+    total,
+    status,
+    updated,
+  } = props.pageControl.orderDetails;
+
+  return (
+    <Box>
+      <form className={classes.root}>
+        <Typography variant="h6" className={classes.title} gutterBottom>
+          Order Metadata
+        </Typography>
+        <Box>
+          <TextField
+            id="order-id"
+            label="Unique ID"
+            defaultValue={orderId}
+            InputProps={{
+              readOnly: true,
+            }}
+            disabled
+            variant="outlined"
+            className={classes.textfield}
+          />
+          <TextField
+            id="state"
+            label="State"
+            defaultValue="New"
+            InputProps={{
+              readOnly: true,
+            }}
+            disabled
+            variant="outlined"
+            className={classes.textfield}
+          />
+          <TextField
+            id="state"
+            label="Order Placed"
+            defaultValue={created}
+            InputProps={{
+              readOnly: true,
+            }}
+            disabled
+            variant="outlined"
+            className={classes.textfield}
+          />
+        </Box>
+        <Typography variant="h6" className={classes.subtitle} gutterBottom>
+          Customer Information
+        </Typography>
+        <Box>
+          <TextField
+            id="customer-first-name"
+            label="First Name"
+            defaultValue={firstName}
+            InputProps={{
+              readOnly: true,
+            }}
+            className={classes.textfield}
+            variant="outlined"
+          />
+          <TextField
+            id="customer-first-name"
+            label="Last Name"
+            defaultValue={lastName}
+            InputProps={{
+              readOnly: true,
+            }}
+            className={classes.textfield}
+            variant="outlined"
+          />
+          <TextField
+            id="customer-email"
+            label="Email"
+            style={{ minWidth: "380px" }}
+            defaultValue={email}
+            InputProps={{
+              readOnly: true,
+            }}
+            variant="outlined"
+            className={classes.textfield}
+          />
+        </Box>
+        <Box>
+          <TextField
+            id="customer-address"
+            label="Delivery Address"
+            style={{ minWidth: "380px" }}
+            defaultValue={address}
+            InputProps={{
+              readOnly: true,
+            }}
+            variant="outlined"
+            className={classes.textfield}
+          />
+          <TextField
+            id="customer-city"
+            label="City"
+            style={{ minWidth: "380px" }}
+            defaultValue={city}
+            InputProps={{
+              readOnly: true,
+            }}
+            variant="outlined"
+            className={classes.textfield}
+          />
+        </Box>
+        <Box>
+          <TextField
+            id="customer-zip"
+            label="ZIP/Postal Code"
+            defaultValue={zip}
+            InputProps={{
+              readOnly: true,
+            }}
+            variant="outlined"
+            className={classes.textfield}
+          />
+          <TextField
+            id="customer-country"
+            label="City"
+            defaultValue={country}
+            InputProps={{
+              readOnly: true,
+            }}
+            variant="outlined"
+            className={classes.textfield}
+          />
+        </Box>
+        <Box display="flex" justifyContent="flex-end">
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+          >
+            Begin Processing
+            <PostAdd className={classes.rightIcon} />
+          </Button>
+        </Box>
+      </form>
+    </Box>
+  );
+}
