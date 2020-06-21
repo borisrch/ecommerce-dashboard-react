28a762813b7104a6d1c34537288309d528487f17
Boris Chan <borisc1996@gmail.com>
2020-06-20 23:04:02 -0400

    prettier code fixes

--- src/Board/Column.js
+++ src/Board/Column.js
@@ -1,51 +1,51 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import { Typography } from '@material-ui/core';
-import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
-import Add from '@material-ui/icons/Add';
-import Grid from '@material-ui/core/Grid';
-import blue from '@material-ui/core/colors/blue';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { Typography } from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import Add from "@material-ui/icons/Add";
+import Grid from "@material-ui/core/Grid";
+import blue from "@material-ui/core/colors/blue";
 
-import Post from './Post';
+import Post from "./Post";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   paper: {
-    // boxShadow: '0 0 1px 0 rgba(0,0,0,.28)',
-    boxShadow: '0 0 11px #eaf0f6',
-    marginBottom: '0.5rem',
-    '&:hover': {
-      backgroundColor: '#e3f2fd',
-      color: 'white',
-    }
+    border: "1px solid #e3f2fd",
+    boxShadow: "0 0 11px #eaf0f6",
+    marginBottom: "0.5rem",
+    "&:hover": {
+      backgroundColor: "#e3f2fd",
+      color: "white",
+    },
   },
   button: {
     backgroundColor: blue[100],
-    '&:hover': {
+    "&:hover": {
       backgroundColor: theme.palette.primary.main,
       "& $addIcon": {
-        color: 'white'
-      }
-    }
+        color: "white",
+      },
+    },
   },
   addIcon: {
-    color: theme.palette.primary.main
-  }
+    color: theme.palette.primary.main,
+  },
 }));
 
 const posts = [
   {
     id: 12,
-    title: 'Review Fall/Winter Product Launch',
-    content: 'Meeting for Adidas 112, Bonobos 325, Calvin Klein 42.',
-    date: 'Oct 8'
+    title: "Review Fall/Winter Product Launch",
+    content: "Meeting for Adidas 112, Bonobos 325, Calvin Klein 42.",
+    date: "Oct 8",
   },
   {
     id: 43,
-    title: 'Organize September 15 Incoming Shipment',
-    content: 'Expecting Hershal, Banana Republic top up.',
-    date: 'Sep 15'
+    title: "Organize September 15 Incoming Shipment",
+    content: "Expecting Hershal, Banana Republic top up.",
+    date: "Sep 15",
   },
 ];
 
@@ -54,19 +54,30 @@ export default function Board(props) {
 
   return (
     <React.Fragment>
-      <Typography variant="h5" gutterBottom style={{ fontFamily: 'ApercuMedium' }}>{props.name}</Typography>
+      <Typography
+        variant="h5"
+        gutterBottom
+        style={{ fontFamily: "ApercuMedium" }}
+      >
+        {props.name}
+      </Typography>
       <Paper className={classes.paper}>
         <Button fullWidth={true} color="primary" className={classes.button}>
-          <Add className={classes.addIcon}/>
+          <Add className={classes.addIcon} />
         </Button>
       </Paper>
-      <Grid container spacing={1} direction="row" justify="center" alignItems="flex-start">
+      <Grid
+        container
+        spacing={1}
+        direction="row"
+        justify="center"
+        alignItems="flex-start"
+      >
         {posts.map((post) => (
           <Grid item xs={12} key={post.id}>
             <Post title={post.title} content={post.content} date={post.date} />
           </Grid>
-        ))
-        }
+        ))}
       </Grid>
     </React.Fragment>
   );
