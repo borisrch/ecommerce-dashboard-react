ae9663f1add3cb34fb36e44d1b9f6768aa3085ff
Boris Chan <borisc1996@gmail.com>
2020-06-20 23:03:06 -0400

    add outer container to allow double borders (inner container listens to hover); remove elevation error.

--- src/Team/Member.js
+++ src/Team/Member.js
@@ -20,19 +20,22 @@ import Box from "@material-ui/core/Box";
 import getAvatar from "../Common/AnimalAvatars";
 
 const useStyles = makeStyles((theme) => ({
-  card: {
-    // boxShadow: '0 0 1px 0 rgba(0,0,0,.28)',
+  outerCard: {
     boxShadow: "0 0 11px #eaf0f6",
+    border: "1px solid #eaf0f6",
+    "&:hover": {},
+  },
+  card: {
     borderColor: "#fff",
     borderStyle: "solid",
-    borderBottomWidth: "4px",
+    borderBottomWidth: "3px",
     borderLeftWidth: "0px",
     borderTopWidth: "0px",
     borderRightWidth: "0px",
     "&:hover": {
       borderColor: theme.palette.primary.light,
       borderStyle: "solid",
-      borderBottomWidth: "4px",
+      borderBottomWidth: "3px",
     },
   },
   media: {
@@ -73,38 +76,43 @@ export default function Member(props) {
   };
 
   return (
-    <Card className={classes.card}>
-      <CardHeader
-        avatar={
-          <Avatar
-            aria-label="avatar"
-            className={classes.avatar}
-            src={props.member.avatar}
-          ></Avatar>
-        }
-        action={
-          <IconButton aria-label="settings" onClick={handleClick}>
-            <ExpandMoreIcon />
-          </IconButton>
-        }
-        title={props.title}
-        subheader={props.date}
-      />
-      <CardContent style={{ paddingTop: "0px" }}>
-        <Typography variant="h6" className={classes.name}>
-          {props.member.firstName} {props.member.lastName}
-        </Typography>
-        <Typography variant="subtitle2" className={classes.subtitle}>
-          {props.member.role}
-        </Typography>
-        <Divider light style={{ marginTop: "0.5em", marginBottom: "0.5em" }} />
-        <Typography variant="subtitle2" className={classes.subtitle}>
-          {props.member.email}
-        </Typography>
-        <Typography variant="subtitle2" className={classes.subtitle}>
-          {props.member.phone}
-        </Typography>
-      </CardContent>
+    <Card className={classes.outerCard}>
+      <Card className={classes.card} elevation={0}>
+        <CardHeader
+          avatar={
+            <Avatar
+              aria-label="avatar"
+              className={classes.avatar}
+              src={props.member.avatar}
+            ></Avatar>
+          }
+          action={
+            <IconButton aria-label="settings" onClick={handleClick}>
+              <ExpandMoreIcon />
+            </IconButton>
+          }
+          title={props.title}
+          subheader={props.date}
+        />
+        <CardContent style={{ paddingTop: "0px" }}>
+          <Typography variant="h6" className={classes.name}>
+            {props.member.firstName} {props.member.lastName}
+          </Typography>
+          <Typography variant="subtitle2" className={classes.subtitle}>
+            {props.member.role}
+          </Typography>
+          <Divider
+            light
+            style={{ marginTop: "0.5em", marginBottom: "0.5em" }}
+          />
+          <Typography variant="subtitle2" className={classes.subtitle}>
+            {props.member.email}
+          </Typography>
+          <Typography variant="subtitle2" className={classes.subtitle}>
+            {props.member.phone}
+          </Typography>
+        </CardContent>
+      </Card>
     </Card>
   );
 }
